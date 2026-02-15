fd0aa72330da5060ad2bacfe9c9b3a83c07b5f92
SakshyamB <sakshyamb23@gmail.com>
2026-02-15 17:54:12 +0545

    Added proper respomsive NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import { CiSearch } from "react-icons/ci";
+import { MdOutlineWbSunny } from "react-icons/md";
+import { FaHome } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { IoMoonOutline } from "react-icons/io5";
+import { CiBellOn } from "react-icons/ci";
+import { CgProfile } from "react-icons/cg";
 const Navbar = () => {
+  const [Darkmode, setDarkmode] = useState(false)
   return (
     <div className='w-full bg-black h-[15%] flex justify-between'>
-        <h1 className='text-bold italic m-2 text-white text-2xl hidden sm:block'>
+      <Link to="/">
+        <h1 className='text-bold italic my-2 mx-1 text-white text-2xl hidden sm:block'>
             SocialApp
         </h1>
-        <h1 className='flex shadow-2xl justify-center w-full my-1'>
+        </Link>
+        <div className='flex justify-center px-2 ml-1 sm:ml-2 my-1'>
+          <Link to="/">
+          <FaHome className='text-white bold my-2 mx-2 sm:mx-4 text-2xl'/>
+          </Link>
+          {Darkmode ? <MdOutlineWbSunny onClick={()=> setDarkmode(false)} className='text-white bold my-2 text-2xl'/>:
+           <IoMoonOutline onClick={()=> setDarkmode(true)} className='text-white bold my-2 text-2xl'/>
+            }
+          </div>
+        <h1 className='flex shadow-2xl mx-2 md:justify-center sm:mx-4 w-full my-1'>
           <CiSearch className='text-2xl text-bold my-2 text-white' />
-          <input type="text" placeholder='Search for contents...' className='w-[70%] sm:w-[50%] outline-1 text-xl rounded-md p-1 text-white outline-white' />
+          <input type="text" placeholder='Search for contents...' className='w-[70%] sm:w-[60%] outline-1 text-xl rounded-md p-1 text-white outline-white' />
+          <button className='bg-blue-500 hidden sm:block w-fit mx-2 text-white my-1 px-1 rounded-xl text-xl sm:text-2xl'>Search</button>
         </h1>
+                <div className='flex justify-center mr-1 sm:mr-2 my-1'>
+          <CiBellOn className='text-white bold my-2 mx-2 sm:mx-4 text-2xl'/>
+          <CgProfile className='text-white bold my-2 text-2xl'/>
+          </div>
     </div>
   )
 }
